66711492dec2a5524609ee88700614071cdfed85
michalsidorow <sztucznymail123@wp.pl>
2020-03-30 19:39:26 +0200

    Initializing project

---
+++ src/main/java/guru/springframework/wetapp/WetApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.wetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WetApplication.class, args);
+	}
+}
---
+++ src/test/java/guru/springframework/wetapp/WetApplicationTests.java
@@ -0,0 +1,16 @@
+package guru.springframework.wetapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WetApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
