121df43ea7290cef387265c80f1bbcf98a9fa7c0
michalsidorow <sztucznymail123@wp.pl>
2020-03-30 19:54:49 +0200

    Creating POJOs. Closes #2

---
+++ src/main/java/guru/springframework/wetapp/Entity/Owner.java
@@ -0,0 +1,4 @@
+package guru.springframework.wetapp.Entity;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/guru/springframework/wetapp/Entity/Wet.java
@@ -0,0 +1,4 @@
+package guru.springframework.wetapp.Entity;
+
+public class Wet extends Person{
+}
